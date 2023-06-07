f3cb125df02fea89604af47d1871c51ba654e307
wkfyd <wkfyd8841@naver.com>
2023-06-07 14:30:22 +0900

    test1

--- MFCApplication1/Cactus.cpp
+++ MFCApplication1/Cactus.cpp
@@ -1,2 +1,50 @@
 #include "pch.h"
 #include "Cactus.h"
+#include "pch.h"
+#include "Cactus.h"
+
+Cactus::Cactus() {
+	x = startX;
+	y = 350;
+	width = 60;
+	height = 100;
+
+	two_X = startX + 500;
+	two_Y = 350;
+	two_Width = 120;
+	two_Height = 100;
+}
+
+void Cactus::ImageLoadCactus() {
+	image[0].Load(L"images\\Cactus.png");
+	image[1].Load(L"images\\CactusDouble.png");
+}
+
+void Cactus::DrawCactus(CDC& dc) {
+	image[0].Draw(dc, x, y, width, height);
+	image[1].Draw(dc, two_X, two_Y, two_Width, two_Height);
+}
+
+void Cactus::Tick() {
+	x -= 16;
+	two_X -= 16;
+
+	if (x <= -100) {
+		x = startX;
+	}
+
+	if (two_X <= -100) {
+		two_X = startX;
+	}
+
+}
+
+void Cactus::GetRect(CRect& rect) {
+
+	rect.SetRect(x, y, x + width, y + height);
+}
+
+void Cactus::GetRect01(CRect& rect) {
+
+	rect.SetRect(two_X, two_Y, two_X + two_Width, two_Y + two_Height);
+}
--- MFCApplication1/Cactus.h
+++ MFCApplication1/Cactus.h
@@ -1,5 +1,25 @@
 #pragma once
 class Cactus
 {
+	//한 개 짜리
+	int width, height, x, y;
+
+	//두 개 짜리
+	int two_Width, two_Height, two_X, two_Y;
+
+	int startX = 1150;
+
+	CRect rect;
+	CImage image[2];
+
+
+public:
+	Cactus();
+
+	void ImageLoadCactus();
+	void DrawCactus(CDC& dc);
+	void Tick();
+	void GetRect(CRect& rect);
+	void GetRect01(CRect& rect);
 };
 
